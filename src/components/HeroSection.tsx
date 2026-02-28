ae2d2291857e3815f0fa9aef2df733e8bc6c7929
apucsd <apusutradhar77@gmail.com>
2026-03-01 00:34:50 +0600

    changes some typegraphy in hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,3 +1,6 @@
+import { CiLocationOn, CiSearch } from "react-icons/ci";
+import { IoIosArrowDown } from "react-icons/io";
+
 const HeroSection = () => {
 
     return (
@@ -29,32 +32,25 @@ const HeroSection = () => {
                         </p>
 
                         {/* Search Bar */}
-                        <div className="bg-white p-3 rounded-2xl shadow-2xl shadow-blue-900/10 border border-slate-100 flex flex-col md:flex-row items-center gap-2 max-w-3xl mb-8">
-                            <div className="flex-grow flex items-center px-4 gap-4 w-full border-b md:border-b-0 md:border-r border-slate-100">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-slate-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                                </svg>
+                        <div className="bg-white p-3   shadow-2xl shadow-blue-900/10  flex flex-col md:flex-row items-center gap-4 max-w-3xl mb-8">
+                            <div className="flex-grow  border-b border-slate-200 flex items-center px-4 gap-4 w-full">
+                                <CiSearch size={24} className="text-slate-700" />
                                 <input
                                     type="text"
                                     placeholder="Job title or keyword"
-                                    className="w-full py-4 outline-none text-slate-800 font-medium placeholder:text-slate-400"
+                                    className="w-full py-4 outline-none text-slate-800 text-sm placeholder:text-slate-400"
                                 />
                             </div>
-                            <div className="flex-grow flex items-center px-4 gap-4 w-full">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-slate-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-                                </svg>
-                                <select className="w-full py-4 outline-none text-slate-800 font-medium bg-transparent appearance-none">
+                            <div className="flex-grow flex items-center px-4 gap-4 w-full border-b border-gray-200">
+                                <CiLocationOn size={24} className="text-slate-700" />
+                                <select className="w-full py-4 outline-none text-slate-800 text-sm bg-transparent appearance-none">
                                     <option>Florence, Italy</option>
                                     <option>Remote</option>
                                     <option>New York, USA</option>
                                 </select>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-slate-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="19 9l-7 7-7-7" />
-                                </svg>
+                                <IoIosArrowDown size={24} className="text-slate-700" />
                             </div>
-                            <button className="w-full md:w-auto bg-[#4A3AFF] hover:bg-[#3b2ecc] text-white px-10 py-5 rounded-sm font-bold transition-all shadow-xl shadow-blue-600/20 whitespace-nowrap">
+                            <button className="w-full cursor-pointer md:w-auto bg-[#4A3AFF] hover:bg-[#3b2ecc] text-white px-10 py-5  font-bold transition-all shadow-xl shadow-blue-600/20 whitespace-nowrap">
                                 Search my job
                             </button>
                         </div>
